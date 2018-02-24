1de9f82b9c1d9880d150ed676c1cbfcc681d1677
Adam <adam.98@yandex.ru>
2018-02-24 20:08:41 +0300

    24.02

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-for (var i=0; i<10; i++) { console.log('hello world') }
+var todoElements = [];
+todoElements.render = function () {
+   code = ''
+   this.forEach(function(element, index) { code +=  '<div class="body">' + '<input type="radio" id="'+index+'" name="'+index+'" class="left" />' + '<label for="' + index + '"><span></span></label>' + '<input type="radio" id="'+index+1 +'" name="'+index+'" class="right" />' + '<label for="' + index+1 + '"><span></span></label>' +
+   element.text + '</div>'})
+   document.getElementById('table').innerHTML = code;
+   document.getElementById('Task').value = '';
+}
+
+
+function Addiction() {
+	
+	
+var text = document.getElementById('Task').value;
+var obj = {text: text};
+todoElements.push(obj)
+todoElements.render()
+
+}
