f03264b85af8787982ca873145dfd0ecde24f76f
Adam <adam.98@yandex.ru>
2018-03-14 23:00:19 +0300

    Галочки работают, но undefined

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
 var todoElements = [];
-
+var obj = {};
 
 todoElements.render = function () {
+ 
    code = ''
-   this.forEach(function(element, index) { code += '<div class="body">' + '<span class="eltext">' + '<input type="radio" id="'+index+'" name="'+index+'" class="left" />' + 
-   '<label for="' + index + '"><span></span></label>' + '<input type="radio" id="'+index+1 +'" name="'+index+'" class="right" />' + '<label for="' + index+1 + '"><span></span></label>' + 
-   element.text + '</span>'+ '</div>'})
-   document.getElementById('table').innerHTML = document.getElementById('table').innerHTML='<div class="top"><img src="checked.jpg"> - In progress <span class="toppic"></span> - Done</div><div class="top1">To Do List</div>' + 
-   code +  '<div class="bots"><div class="bot"><input type="text" class="text1" id="Task"></div><div class="bot1">'+'<input type="button" class ="button" value="Добавить" id="add" onclick='+'Addiction()'+'> </div>';
+   this.forEach(function(element, index) {
+	   if (element.done == 'one') {code += '<div class="body">' + '<span class="eltext">' + '<input type="radio" checked onclick="one()" id="'+index+
+   '" name="'+index+'" class="left"/>' + '<label for="' + index + '"><span></span></label>' + '<input type="radio" id="'+index+1 +
+   '" name="'+index+'" class="right" onclick="two()" />' + '<label for="' + index+1 + '"><span></span></label>' + 
+   element.text + '</span>'+ '</div>'}
+
+   else if (element.done == 'two') {code += '<div class="body">' + '<span class="eltext">' + '<input type="radio" onclick="one()" id="'+index+
+   '" name="'+index+'" class="left" />' + '<label for="' + index + '"><span></span></label>' + '<input type="radio" id="'+index+1 +
+   '" name="'+index+'" class="right" checked onclick="two()" />' + '<label for="' + index+1 + '"><span></span></label>' + 
+   element.text + '</span>'+ '</div>'}
+   
+   else{
+   code += '<div class="body">' + '<span class="eltext">' + '<input type="radio"  onclick="one()" id="'+index+
+   '" name="'+index+'" class="left"/>' + '<label for="' + index + '"><span></span></label>' + '<input type="radio" id="'+index+1 +
+   '" name="'+index+'" class="right" onclick="two()" />' + '<label for="' + index+1 + '"><span></span></label>' + 
+   element.text + '</span>'+ '</div>'}})
+   document.getElementById('table').innerHTML='<div class="top"><img src="checked.jpg">'+
+   ' - In progress <span class="toppic"></span> - Done</div><div class="top1">To Do List</div>' + 
+   code +  '<div class="bots"><div class="bot"><input type="text" class="text1" id="Task"></div><div class="bot1">'+
+   '<input type="button" class ="button" value="Добавить" id="add" onclick='+'Addiction()'+'> </div>';
    document.getElementById('Task').value = '';
 }
 
+function two(element) {
+	obj.done = 'two';
+	todoElements.render();
+};
+
+function one(element) {
+	element.done = 'one';
+	todoElements.render();
+};
 
 function Addiction() {
 	
@@ -18,7 +43,7 @@ function Addiction() {
 var text = document.getElementById('Task').value;
 if(text == "") {alert('Сначала напиши задание!')}
 	else {
-	var obj = {text: text};
+	obj = {text: text};
 todoElements.push(obj)
 	todoElements.render()}
 
