f0e43f7444ba66bc8d625c79bd461297b3fb078a
Adam <adam.98@yandex.ru>
2018-03-19 12:35:17 +0300

    Added del buttons

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ var obj = {};
 todoElements.check = function (){
 	this.forEach(function(element,index) {var radio = document.getElementsByName(index);
 	if(radio[0].checked==true){element.done = 'one';}
-else if(radio[1].checked==true){element.done= 'two';};})};
+else if(radio[1].checked==true){element.done= 'two';}
+else if(radio[2].checked==true){todoElements.splice(index, 1); todoElements.render();}})};
 
 todoElements.render = function () {
    code = ''
@@ -12,20 +13,23 @@ todoElements.render = function () {
 	   if (element.done == 'one') {code += '<div class="body">' + '<span class="eltext">' + '<input type="radio" checked onclick="todoElements.check()" id="'+index+
    '" name="'+index+'" class="left"/>' + '<label for="' + index + '"><span></span></label>' + '<input type="radio" id="'+index+1 +
    '" name="'+index+'" class="right" onclick="todoElements.check()" />' + '<label for="' + index+1 + '"><span></span></label>' + 
+   '<input type="radio" onclick="todoElements.check()" id="'+index+2+'" name="'+index+'" class="rr"/>'+'<label for="' + index+2 + '"><span></span></label>' +
    element.text + '</span>'+ '</div>'}
 
    else if (element.done == 'two') {code += '<div class="body">' + '<span class="eltext">' + '<input type="radio" onclick="todoElements.check()" id="'+index+
    '" name="'+index+'" class="left" />' + '<label for="' + index + '"><span></span></label>' + '<input type="radio" id="'+index+1 +
    '" name="'+index+'" class="right" checked onclick="todoElements.check()" />' + '<label for="' + index+1 + '"><span></span></label>' + 
+   '<input type="radio" onclick="todoElements.check()" id="'+index+2+'" name="'+index+'" class="rr"/>'+'<label for="' + index+2 + '"><span></span></label>' +
    element.text + '</span>'+ '</div>'}
    
    else{
    code += '<div class="body">' + '<span class="eltext">' + '<input type="radio"  onclick="todoElements.check()" id="'+index+
    '" name="'+index+'" class="left"/>' + '<label for="' + index + '"><span></span></label>' + '<input type="radio" id="'+index+1 +
    '" name="'+index+'" class="right" onclick="todoElements.check()" />' + '<label for="' + index+1 + '"><span></span></label>' + 
-   element.text + '</span>'+ '</div>'}})
+   '<input type="radio" onclick="todoElements.check()" id="'+index+2+'" name="'+index+'" class="rr"/>'+'<label for="' + index+2 + '"><span></span></label>' +
+   element.text + '</span></div>'}})
    document.getElementById('table').innerHTML='<div class="top"><img src="checked.jpg">'+
-   ' - In progress <span class="toppic"></span> - Done</div><div class="top1">To Do List</div>' + 
+   ' - In progress <span class="toppic"></span> - Done <img src="x.jpg"> - Delete</div><div class="top1">To Do List</div>' + 
    code +  '<div class="bots"><div class="bot"><input type="text" class="text1" id="Task"></div><div class="bot1">'+
    '<input type="button" class ="button" value="Добавить" id="add" onclick='+'Addiction()'+'> </div>';
    document.getElementById('Task').value = '';
--- script2.js
+++ script2.js
@@ -4,33 +4,39 @@ var tasks = '';
 
 todoElements.check = function (){
 	this.forEach(function(element,index) {var radio = document.getElementsByName(index);
+	
 	if(radio[0].checked==true){element.done = 'one';}
-else if(radio[1].checked==true){element.done= 'two';};})};
+else if(radio[1].checked==true){element.done = 'two';}
+else if(radio[2].checked==true){console.log(index, ' ', index+1);todoElements.splice(index, 1); todoElements.render()};})};
 
 todoElements.render = function () {
    this.forEach(function(element, index) {
-	   if (element.done == 'one') {element.code = '<div class="body" name="c" id="c'+index+'">' + '<span class="eltext">' + '<input type="radio" checked onclick="todoElements.check()" id="'+index+
+	   
+	  if (element.done == 'one') {element.code = '<div class="body">' + '<span class="eltext">' + '<input type="radio" checked onclick="todoElements.check()" id="'+index+
    '" name="'+index+'" class="left" />' + '<label for="' + index + '"><span></span></label>' + '<input type="radio" id="'+index+1 +
    '" name="'+index+'" class="right" onclick="todoElements.check()" />' + '<label for="' + index+1 + '"><span></span></label>' + 
+   '<input type="radio" onclick="todoElements.check()" id="'+index+2+'" name="'+index+'" class="rr"/>'+'<label for="' + index+2 + '"><span></span></label>' +
    element.text + '</span>'+ '</div>'}
 
-   else if (element.done == 'two') {element.code = '<div class="body" name="c" id="c'+index+'">' + '<span class="eltext">' + '<input type="radio" onclick="todoElements.check()" id="'+index+
+   else if (element.done == 'two') {element.code = '<div class="body">' + '<span class="eltext">' + '<input type="radio" onclick="todoElements.check()" id="'+index+
    '" name="'+index+'" class="left" />' + '<label for="' + index + '"><span></span></label>' + '<input type="radio" id="'+index+1 +
    '" name="'+index+'" class="right" checked onclick="todoElements.check()" />' + '<label for="' + index+1 + '"><span></span></label>' + 
+   '<input type="radio" onclick="todoElements.check()" id="'+index+2+'" name="'+index+'" class="rr"/>'+'<label for="' + index+2 + '"><span></span></label>' +
    element.text + '</span>'+ '</div>'}
    
    else{ 
-   element.code = '<div class="body" name"c" id="c'+index+'">' + '<span class="eltext">' + '<input type="radio" id="'+index+
+   element.code = '<div class="body">' + '<span class="eltext">' + '<input type="radio" id="'+index+
    '" name="'+index+'" class="left" onclick="todoElements.check()"/>' + '<label for="' + index + '"><span></span></label>' + '<input type="radio" id="'+index+1 +
    '" name="'+index+'" class="right" onclick="todoElements.check()" />' + '<label for="' + index+1 + '"><span></span></label>' + 
-   element.text + '</span>'+ '</div>'}; })
+   '<input type="radio" onclick="todoElements.check()" id="'+index+2+'" name="'+index+'" class="rr"/>'+'<label for="' + index+2 + '"><span></span></label>' +
+	 element.text + '</span>'+ '</div>'};} )
    function Lcode () {
 	   for(var i = 0; i < todoElements.length; i++) {var line = todoElements[i].code; var allLines = allLines + line + " ";}
 	   tasks = allLines;
 	   };
    Lcode();
    document.getElementById('table').innerHTML='<div class="top"><img src="checked.jpg">'+
-   ' - In progress <span class="toppic"></span> - Done</div><div class="top1">To Do List</div>' + 
+   ' - In progress <span class="toppic"></span> - Done <img src="checked.jpg"> - Delete</div><div class="top1">To Do List</div>' + 
    tasks +  '<div class="bots"><div class="bot"><input type="text" class="text1" id="Task"></div><div class="bot1">'+
    '<input type="button" class ="button" value="Добавить" id="add" onclick='+'Addiction()'+'> </div>';
    document.getElementById('Task').value = '';
@@ -42,6 +48,7 @@ var text = document.getElementById('Task').value;
 if(text == "") {alert('Сначала напиши задание!')}
 	else {
 	obj = {text: text,
+	del: false
 	};
 todoElements.push(obj)
 	todoElements.render()}
