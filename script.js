c50ca0d71620e40f36d1da61f82f27b83256e3f8
Adam <adam.98@yandex.ru>
2018-03-25 13:34:25 +0300

    New Architecture

--- script.js
+++ script.js
@@ -1,47 +1,138 @@
-var todoElements = [];
-var obj = {};
-
-todoElements.check = function (){
-	this.forEach(function(element,index) {var radio = document.getElementsByName(index);
-	if(radio[0].checked==true){element.done = 'in progress';}
-else if(radio[1].checked==true){element.done= 'done';}
-else if(radio[2].checked==true){todoElements.splice(index, 1); todoElements.render();}
-})};
-
-todoElements.render = function () {
-   code = ''
-   this.forEach(function(element, index) {
-	   if (element.done == 'in progress') {code += '<div class="body">' + '<span class="eltext">' + '<input type="radio" checked onclick="todoElements.check()" id="'+index+
-   '" name="'+index+'" class="inProgress"/>' + '<label for="' + index + '"><span></span></label>' + '<input type="radio" id="'+index+1 +
-   '" name="'+index+'" class="done" onclick="todoElements.check()" />' + '<label for="' + index+1 + '"><span></span></label>' + 
-   '<input type="radio" onclick="todoElements.check()" id="'+index+2+'" name="'+index+'" class="delete"/>'+'<label for="' + index+2 + '"><span></span></label>' +
-   element.text + '</span>'+ '</div>'}
-
-   else if (element.done == 'done') {code += '<div class="body">' + '<span class="eltext">' + '<input type="radio" onclick="todoElements.check()" id="'+index+
-   '" name="'+index+'" class="inProgress" />' + '<label for="' + index + '"><span></span></label>' + '<input type="radio" id="'+index+1 +
-   '" name="'+index+'" class="done" checked onclick="todoElements.check()" />' + '<label for="' + index+1 + '"><span></span></label>' + 
-   '<input type="radio" onclick="todoElements.check()" id="'+index+2+'" name="'+index+'" class="delete"/>'+'<label for="' + index+2 + '"><span></span></label>' +
-   element.text + '</span>'+ '</div>'}
-   
-   else{
-   code += '<div class="body">' + '<span class="eltext">' + '<input type="radio"  onclick="todoElements.check()" id="'+index+
-   '" name="'+index+'" class="inProgress"/>' + '<label for="' + index + '"><span></span></label>' + '<input type="radio" id="'+index+1 +
-   '" name="'+index+'" class="done" onclick="todoElements.check()" />' + '<label for="' + index+1 + '"><span></span></label>' + 
-   '<input type="radio" onclick="todoElements.check()" id="'+index+2+'" name="'+index+'" class="delete"/>'+'<label for="' + index+2 + '"><span></span></label>' +
-   element.text + '</span></div>'}})
-   
-   document.getElementById('table').innerHTML='<div class="top"><img src="checked.jpg">'+
-   ' - In progress <span class="toppic"></span> - Done <img src="x.jpg"> - Delete</div><div class="top1">To Do List</div>' + 
-   code +  '<div class="bots"><div class="bot"><input type="text" class="text1" id="Task"></div><div class="bot1">'+
-   '<input type="button" class ="button" value="Добавить" id="add" onclick='+'Addiction()'+'> </div>';
-   document.getElementById('Task').value = '';
-}
+ var obj = {};
+ var appData = {rows: todoElements=[], filter: 1}; 
 
 function Addiction() {	
-var text = document.getElementById('Task').value;
-if(text == "") {alert('Сначала напиши задание!')}
+    var text = document.getElementById('Task').value;
+    if(text == "") {alert('Сначала напиши задание!')}
 	else {
 	obj = {text: text};
-todoElements.push(obj)
-	todoElements.render()}
+    appData.rows.push(obj)
+	render();}
 }
+
+function inProgress(){appData.rows[this.id].done='in progress';};
+function done(){var a=(this.id-'1')/10;appData.rows[a].done='done';};
+function del(){var a=(this.id-'2')/10;appData.rows.splice(a,1); render();};
+
+function filterBy() {
+	var filter = document.getElementById("Filter").options.selectedIndex;
+	appData.filter = filter;
+    render();
+}
+
+  render = function () {
+	var table = document.getElementById('table');
+	var top = document.createElement("div");
+	var top1 = document.createElement("div");
+	var top2 = document.createElement("div");
+	var select = document.createElement("select");
+	var bot = document.getElementById('bots');
+	var options = ["Filter By","All","In Progress","Done"];
+	
+  for (var i = 0; i < options.length; i++) {
+    var option = document.createElement("option");
+    option.value = options[i];
+    option.text = options[i];
+	if (i===0){option.disabled = true;};
+    select.appendChild(option);
+}
+	if (appData.filter == 1) {select.selectedIndex = 1}
+	else if (appData.filter == 2) {select.selectedIndex = 2}
+	else if (appData.filter == 3) {select.selectedIndex = 3}
+	
+	   select.id = 'Filter';
+	   select.setAttribute('size', 1);
+	   top.className = 'top';
+	   top.innerHTML = '<img src="checked.jpg"> - In progress <span class="toppic"></span> - Done <img src="x.jpg"> - Delete';
+	   top1.className = 'top1';
+	   top1.innerHTML ='To Do List';
+	   top2.className = 'top1';
+	   top2.appendChild(select);
+	   
+	  
+	   
+  while (table.firstChild.className != 'bots') {table.removeChild(table.firstChild)};
+	   table.insertBefore(top2, bot);
+	   table.insertBefore(top1, top2);
+	   table.insertBefore(top, top1);
+	
+  appData.rows.forEach(function(element, index) {
+    var newDiv = document.createElement("div");
+	var spanT = document.createElement("span");
+	var radio = document.createElement("input");
+	var radio1 = document.createElement("input");
+	var radio2 = document.createElement("input");
+	var label = document.createElement("label");
+	var label1 = document.createElement("label");
+	var label2 = document.createElement("label");
+	
+       label.setAttribute('for', index);
+	   label1.setAttribute('for', index+'1');
+	   label2.setAttribute('for', index+'2');
+	
+	   radio.type = "radio";
+	   radio.name = index;
+	   radio.id = index;
+	   radio.className = "inProgress";
+	   
+	   radio1.type = "radio";
+	   radio1.name = index;
+	   radio1.id = index+'1';
+	   radio1.className = "done";
+	   
+	   radio2.type = "radio";
+	   radio2.name = index;
+	   radio2.id = index+'2';
+	   radio2.className = "delete";
+	   
+	   newDiv.className = 'body';
+	   spanT.className = 'eltext';
+	   
+    if(element.done == 'done') { radio1.setAttribute('checked', true);}
+    else if (element.done == 'in progress') { radio.setAttribute('checked', true);};
+	   
+	   label.innerHTML='<span></span>';
+	   label1.innerHTML='<span></span>';
+	   label2.innerHTML='<span></span>';
+	  
+	   spanT.appendChild(radio);
+	   spanT.appendChild(label);
+	   spanT.appendChild(radio1);
+	   spanT.appendChild(label1);
+	   spanT.appendChild(radio2);
+	   spanT.appendChild(label2);
+	   
+	if (appData.filter == 2) {
+	   if(element.done == 'in progress'){
+	   spanT.innerHTML += element.text;
+       newDiv.appendChild(spanT);
+       table.insertBefore(newDiv, bot);
+	    document.getElementById(index).addEventListener("click", inProgress, false);
+        document.getElementById(index+'1').addEventListener("click", done, false);
+        document.getElementById(index+'2').addEventListener("click", del, false);
+	   }
+	}
+	else if (appData.filter == 3) {
+	   if(element.done == 'done'){
+	   spanT.innerHTML += element.text;
+       newDiv.appendChild(spanT);
+       table.insertBefore(newDiv, bot);
+	    document.getElementById(index).addEventListener("click", inProgress, false);
+        document.getElementById(index+'1').addEventListener("click", done, false);
+        document.getElementById(index+'2').addEventListener("click", del, false);
+	   }
+	}
+	else if (appData.filter == 1) {
+	   spanT.innerHTML += element.text;
+       newDiv.appendChild(spanT);
+       table.insertBefore(newDiv, bot);
+	    document.getElementById(index).addEventListener("click", inProgress, false);
+        document.getElementById(index+'1').addEventListener("click", done, false);
+        document.getElementById(index+'2').addEventListener("click", del, false);
+	}
+  }) 
+        document.getElementById('Filter').addEventListener("change", filterBy, false);  
+        document.getElementById('Task').value = '';  
+ }
+ 
+        document.getElementById("add").addEventListener("click", Addiction, false);
